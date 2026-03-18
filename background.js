5d865bfdeb701c2228306e80b40030fa770aacf7
aragocz <contact@aragocz.com>
2026-03-18 23:25:58 +0100

    Design, special cases framework

--- background.js
+++ background.js
@@ -1,25 +1,45 @@
 try{
+    const browser = chrome; // For Chromium browser < 146
     const extid = browser.runtime.id;
-    var currtab;
-    var currurl;
-    var attackurl;
-    var tabiddd;
-
+    const timeregex = /^\W*(?:(?<TIME24>(?:[01]?\d|2[0-3])(?::[0-5]?\d){1,2})|(?:T?(?<TIMEUNI>(?:[01]?\d|2[0-3]):[0-5]?\d(?::[0-5]?\d)?)(?:(?<MODUNI>\+|\-)(?<UNIOFF>[01](?:1[0-4]|\d):[0-5]\d)))|(?:(?<TIME12>(?:(?:1[012])|[1-9]))(?::[0-5]?\d){0,2} ?(?<AMPM>AM|PM|am|pm))|(?:(?<TIMEOFF>(?:[01]?\d|2[0-3])(?::[0-5]?\d){0,2}) ?(?:(?:GMT|UTC|gmt|utc)(?<MODOFF>\+|\-)(?<OFF>(?:1[0-4]|\d)(?::[0-5]?\d){0,2})))) ?(?:(?<TZ>[A-Za-z]{1,4})|(?<TZLONG>(?:[A-Z][a-z]+ )+Time))?\W*$/;
+    const urlregex = /^\W*(?<URL>[a-z]+:\/{2,3}[a-z0-9-]+(?:\.[a-z0-9-]+)+(?:\/[a-z0-9-_.~?#&=;,!$'()*+%]*)*)\W*$/i;
 
     browser.runtime.onInstalled.addListener(() => {
         //browser.tabs.create({ url: "browser-extension://"+extid+"/extensibles/options.html"})
         //browser.storage.local.set({apikey: "YOURKEYHERE"})
     });
 
-    function pageLoad(tabId, changeInfo, tab){
-        /*if(changeInfo.status === "complete"){
-            
-        }*/
-       tab
-    }
-
-    browser.tabs.onUpdated.addListener(pageLoad)
-
+    
+    browser.runtime.onMessage.addListener(handleMessages);
 }catch(e) {console.log(e)}
 
+function handleMessages(message, _sender, respond){
 
+    switch(message.type){
+        case "search":
+            //invoke search API
+        break;
+
+        case "check":
+            respond(checkForSpecial(message.selection));
+        break;
+    }
+
+    return true;
+}
+
+function checkForSpecial(data = ""){
+    if(urlregex.test(data)){
+        return {type:0x1, match: data.match(urlregex).groups.url};
+    }else if(timeregex.test(data)){
+        const match = data.match(timeregex);
+        //CALCULATE TIME HERE
+        const time = null;
+
+        return {type:0x2, match: time}
+    }
+}
+
+function convertTime(){
+
+}
--- selector.js
+++ selector.js
@@ -4,13 +4,11 @@ let selText = "";
 const overlay = document.createElement("div");
 overlay.id = "selectionOverlay";
 overlay.innerHTML = `
-  <div class="ext-tooltip-container">
-    <div class="ext-tooltip-btn" id="ext-btn-search">Search</div>
-    <div class="ext-tooltip-divider"></div>
-    <div class="ext-tooltip-btn" id="ext-btn-copy">Copy</div>
-    <div class="ext-tooltip-divider"></div>
-    <div class="ext-tooltip-btn" id="ext-btn-snapshot">Snapshot</div>
-  </div>
+    <div class="ext-tooltip-container">
+        <div class="ext-tooltip-btn" id="ext-btn-search">Search</div>
+        <div class="ext-tooltip-divider"></div>
+        <div class="ext-tooltip-btn" id="ext-btn-copy">Copy</div>
+    </div>
 `;
 
 overlay.addEventListener("mousedown", (e) => e.stopPropagation());
@@ -31,7 +29,7 @@ function evalSelection(){
 
     if(selection.rangeCount <= 0) return;
 
-    const rect = selection.getRangeAt(0).getBoundingClientRect()
+    const rect = selection.getRangeAt(0).getBoundingClientRect();
 
     if(selection.type == "Range" && selText != "" && rect.width > 0){
         createOverlay(rect);
@@ -39,12 +37,35 @@ function evalSelection(){
 }
 
 function createOverlay(rect){
+    sendExtensionMessage({selection: window.getSelection().toString()}, (res) => {
+        console.log(res);
+    });
+    
+
     document.body.appendChild(overlay);
-    overlay.style.left = (rect.left+window.scrollX+(rect.width/2)-(overlay.offsetWidth/2)).toString()+"px"
-    overlay.style.top = (rect.top+window.scrollY-overlay.offsetHeight-14).toString()+"px"
+    const container = overlay.querySelector(".ext-tooltip-container");
+
+    container.classList.remove("inverted");
+
+    if(rect.top > overlay.offsetHeight+14){
+        overlay.style.top = (rect.top+window.scrollY-overlay.offsetHeight-14).toString()+"px";
+    }else {
+        container.classList.add("inverted");
+        overlay.style.top = (rect.bottom+window.scrollY+14).toString()+"px";
+    }
+
+    overlay.style.left = (rect.left+window.scrollX+(rect.width/2)-(overlay.offsetWidth/2)).toString()+"px";
 }
 
 function destroyOverlay(){
     if(!overlay.parentNode) return;
     document.body.removeChild(overlay);
+}
+
+async function sendExtensionMessage(type, obj, cb){
+    const payload = new Object.fromEntries(obj);
+    payload.type = type;
+    const browser = chrome;
+    const res = await browser.runtime.sendMessage(payload);
+    cb(res);
 }
